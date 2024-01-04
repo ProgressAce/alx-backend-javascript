368e9a00a36f6d6d8b4b34d0af9fbd88eaee72a2
ProgressAce <daiyaansallie7@gmail.com>
2024-01-04 06:02:42 +0200

    style: eslint compliance

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
   const employees = reportWithIterator;
-  let employeeNames = "";
+  let employeeNames = '';
   let i = 1;
 
   for (const employee of employees) {
     if (i < employees.length) {
-      employeeNames = employeeNames + `${employee} | `;
+      employeeNames += `${employee} | `;
     } else {
       employeeNames += employee;
     }
