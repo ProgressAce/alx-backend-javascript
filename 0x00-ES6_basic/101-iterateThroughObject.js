8d9ce2b0c4bd33174b16a80ecc116183ee998f97
ProgressAce <daiyaansallie7@gmail.com>
2024-01-04 05:59:21 +0200

    add task solution and main file for testing

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,16 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = reportWithIterator;
+  let employeeNames = "";
+  let i = 1;
+
+  for (const employee of employees) {
+    if (i < employees.length) {
+      employeeNames = employeeNames + `${employee} | `;
+    } else {
+      employeeNames += employee;
+    }
+    i += 1;
+  }
+
+  return employeeNames;
+}
