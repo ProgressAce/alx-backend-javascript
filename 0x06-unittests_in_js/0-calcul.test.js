9ec243d986ff8b73d1e7d2a1892aeac853804964
ProgressAce <daiyaansallie7@gmail.com>
2024-03-02 06:49:47 +0200

    add test case for floating and whole number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,14 @@ const assert = require("assert");
 
 describe("Test function <calculateNumber>.", function () {
   // accepts a and b arguments which are assumed to always be numbers
+  it("1. Returns 7 with input 3.7 and 3", function () {
+    assert.equal(calculateNumber(3.7, 3), 7);
+  });
+
+  it("1. Returns -3 with input -1.7 and -1", function () {
+    assert.equal(calculateNumber(-1.7, -1), -3);
+  });
+
   it("1. Sum positive integers", function () {
     assert.equal(calculateNumber(1, 3), 4, "Equal");
   });
@@ -25,11 +33,6 @@ describe("Test function <calculateNumber>.", function () {
 
     // mixed sign addition
     assert.equal(calculateNumber(5.6, -3.4), 3);
-  });
-
-  it("5. Sum of floating-point and integer numbers", function () {
-    assert.equal(calculateNumber(7.2, 2), 9);
-    assert.equal(calculateNumber(5, -2.1), 3);
   });
 
   it("6. Checking sum to excede maximum and minimum numbers", function () {
