8059114630241291842f7eb594e9b5f1a6300486
ProgressAce <daiyaansallie7@gmail.com>
2024-04-30 14:53:25 +0200

    refactor: remove unnecessary response method calls

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,16 +1,14 @@
 // Controller for the home of the application server.
 class AppController {
-    /**
+  /**
      * Returns the homepage.
-     * 
-     * @param {import("express").Request} request 
-     * @param {import("express").Response} response 
+     *
+     * @param {import("express").Request} request
+     * @param {import("express").Response} response
      */
-    static getHomepage(request , response) {
-        response.status(200);
-        response.send('Hello Holberton School!');
-        response.end();
-    }
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
+  }
 }
 
 export default AppController;
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,92 +2,81 @@
 import readDatabase from '../utils';
 
 class StudentsController {
-    /**
+  /**
      * Gets all the students from the database and returns them
      * as a display message.
-     * 
-     * @param {import('express').Request} request 
-     * @param {import('express').Response} response 
+     *
+     * @param {import('express').Request} request
+     * @param {import('express').Response} response
      */
-    static getAllStudents(request, response) {
+  static getAllStudents(request, response) {
+    const database = process.argv[2];
+    readDatabase(database)
+      .then((data) => {
+        let respBody = 'This is the list of our students\n';
+        const fieldOrder = Object.keys(data);
+        fieldOrder.sort((a, b) => (a.toLowerCase() > b.toLowerCase() ? 1 : -1));
 
-        const database = process.argv[2];
-        readDatabase(database)
-          .then((data) => {
-            let respBody = 'This is the list of our students\n';
-            let fieldOrder = Object.keys(data);
-            fieldOrder.sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1);
+        let fieldIdx = 0;
+        for (const field of fieldOrder) {
+          const fieldSize = data[field].length;
+          respBody += `Number of students in ${field}: ${fieldSize}. `;
 
-            let fieldIdx = 0;
-            for (const field of fieldOrder) {
-                const fieldSize = data[field].length;
-                respBody += `Number of students in ${field}: ${fieldSize}. `;
+          let stdIdx = 0;
+          let studentList = 'List: ';
+          for (const name of data[field]) {
+            studentList += name;
 
-                let stdIdx = 0;
-                let studentList = 'List: ';
-                for (const name of data[field]) {
-                    studentList += name;
+            stdIdx += 1;
+            if (stdIdx < fieldSize) studentList += ', ';
+          }
+          respBody += studentList;
 
-                    stdIdx += 1;
-                    if (stdIdx < fieldSize) studentList += ', ';
-                }
-                respBody += studentList;
+          // so the final field sent does not contain a newline
+          fieldIdx += 1;
+          if (fieldIdx < fieldOrder.length) respBody += '\n';
+        }
+        response.send(200, respBody);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
+  }
 
-                // so the final field sent does not contain a newline
-                fieldIdx += 1;
-                if (fieldIdx < fieldOrder.length) respBody += '\n';
-            }
-            response.status(200);
-            response.send(respBody);
-            response.end();
-          })
-          .catch(() => {
-            response.status(500);
-            response.send('Cannot load the database');
-            response.end();
-          });
-    }
-
-    /**
+  /**
      * Gets all the students of the provided major in the database
      * and returns them as a display message.
-     * 
-     * @param {import('express').Request} request 
-     * @param {import('express').Response} response 
+     *
+     * @param {import('express').Request} request
+     * @param {import('express').Response} response
      */
-    static getAllStudentsByMajor(request, response) {
-        const major = request.params.major;
+  static getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
 
-        if (['CS', 'SWE'].includes(major)) {
-            const database = process.argv[2];
-            readDatabase(database)
-              .then((data) => {
-                const fieldStudents = data[major];
+    if (['CS', 'SWE'].includes(major)) {
+      const database = process.argv[2];
+      readDatabase(database)
+        .then((data) => {
+          const fieldStudents = data[major];
 
-                let i = 0;
-                let studentList = 'List: ';
-                for (const name of fieldStudents) {
-                    studentList += name;
+          let i = 0;
+          let studentList = 'List: ';
+          for (const name of fieldStudents) {
+            studentList += name;
 
-                    i += 1;
-                    if (i < fieldStudents.length) studentList += ', ';
-                }
+            i += 1;
+            if (i < fieldStudents.length) studentList += ', ';
+          }
 
-                response.send(studentList);
-                response.status(200);
-                response.end();
-              })
-              .catch(() => {
-                response.status(500);
-                response.send('Cannot load the database');
-                response.end();
-              });
-        } else {
-            response.status(500);
-            response.send('Major parameter must be CS or SWE');
-            response.end();
-        }
+          response.send(200, studentList);
+        })
+        .catch(() => {
+          response.send(500, 'Cannot load the database');
+        });
+    } else {
+      response.send(500, 'Major parameter must be CS or SWE');
     }
+  }
 }
 
 export default StudentsController;
