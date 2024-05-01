449cc4fe6d06bd5771f673d3923c9d8be124b8e3
ProgressAce <daiyaansallie7@gmail.com>
2024-05-01 14:34:42 +0200

    add: functionality for test practising

---
+++ PrepLivin-Unittests_in_JS/1-calcul.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * 
+ * a and b are assumed to always be numbers, without validating it.
+ * @param {string} type 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+function calculateNumber(type, a, b) {
+    // try/catch can be used
+    a = Math.round(a);
+    b = Math.round(b);
+
+    // ADDITION of a and b
+    if (type === 'SUM') {
+        return a + b;
+    }
+
+    // SUBTRACTION of a and b
+    if (type === 'SUBTRACT') {
+        return a - b;
+    }
+
+    // DIVISION of a and b
+    if (type === 'DIVIDE') {
+        if (b === 0) return 'Error'; //
+
+        return a / b;
+    }
+
+    return;
+}
+
+module.exports = calculateNumber;
