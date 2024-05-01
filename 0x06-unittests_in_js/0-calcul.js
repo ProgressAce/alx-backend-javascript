75f5c1b8d7052abeb46c561456215a83d365dbc6
Daiyaan <96121165+ProgressAce@users.noreply.github.com>
2024-05-01 15:04:21 +0200

    Delete 0x06-unittests_in_js directory
    In order to redo the problem/practise exercises

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,9 +0,0 @@
-function calculateNumber(a, b) {
-  // Rounds two numbers and returns their sum.
-  const num1 = Math.round(a);
-  const num2 = Math.round(b);
-
-  return num1 + num2;
-}
-
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/README.md
+++
@@ -1,3 +0,0 @@
-# Basic unittests in Node JS
-
-Exploring the world of unittesting in NodeJS and with mocha, sinon and chai.
